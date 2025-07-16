c9970ebd501ea9f9017fb0bca39163167c7c0320
ashutoshghodke <ashutoshghodke5600@gmail.com>
2025-07-16 21:39:06 +0530

    day2 class

--- Introduction of python/Day1/day1/day1.py
+++
@@ -1,4 +0,0 @@
-a = 4
-A = "Sally"
-#A will not overwrite a
-print(A)
---
+++ Introduction of python/Day1/day2/day2.py
@@ -0,0 +1,27 @@
+# p="sanket"  # sanket is my manager
+# print(p)
+
+# a=1   # this is an addition of two no 
+# b=2
+# c=4
+# print(a+b+c)
+
+
+# _s = "sanket"
+# P = 9
+# print(_s)     #output=sanket
+
+# print(P)     #output=9
+
+
+# 3="rohini "
+# # print(3)
+
+# print="abc"
+# print(print)
+
+
+# s = "sanket"
+# s = 9
+# s="pradeep"
+# print(s)
