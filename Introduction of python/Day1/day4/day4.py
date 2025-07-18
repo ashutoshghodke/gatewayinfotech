f3126beffc51f540e9e5a7632d839f2145e3bfd1
ashutoshghodke <ashutoshghodke5600@gmail.com>
2025-07-18 22:04:53 +0530

    day4 class

---
+++ Introduction of python/Day1/day4/day4.py
@@ -0,0 +1,43 @@
+# print("""Twinkle, twinkle, little star,
+# How I wonder what you are!
+# Up above the world so high,
+# Like a diamond in the sky.
+# When the blazing sun is gone,
+# When he nothing shines upon,
+# Then you show your little light,
+# Twinkle, twinkle, all the night.
+# Then the trav'ller in the dark,
+# Thanks you for your tiny spark,
+# He couldn't see which way to go,
+# If you did not twinkle so""");
+
+# a="ashutosh"
+# print(len(a))
+
+# v="1234567890"
+# print(len(v))
+
+
+# print("sanaketsalunke") 
+# sanaket salunke ---
+# H  e   l   l    o
+# 0  1   2   3    4
+
+# b = "Hello, World!"
+# print(b[:9])
+
+# b = "Hello, World!"
+# print(b[0:2:9])
+
+
+
+# a="ashutosh"
+# print(a.upper()) 
+
+# a="ASHUTOSH"
+# print(a.lower())
+
+
+a = " Hello, World! "
+# print(a)
+print(a.strip()) # returns "Hello, World!"
