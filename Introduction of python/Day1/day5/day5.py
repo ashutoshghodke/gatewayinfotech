e4696e2152cb87cc0b7230d9ed5fca952af2ebfb
ashutoshghodke <ashutoshghodke5600@gmail.com>
2025-07-22 22:00:38 +0530

    day5 class

---
+++ Introduction of python/Day1/day5/day5.py
@@ -0,0 +1,48 @@
+
+
+
+# print("WELCOME TO THE ELECTION BOARD OF MAHARASHTRA ")
+
+# age=age = int(input("Enter your age: "))
+
+# if age <18 :
+#     print("USER NOT VALID TO VOTE ")
+
+# elif age >50 :
+#     print("USER NOT VALID TO VOTE ")
+
+# else:
+#     print("USER IS VALID TO VOTE ")
+
+
+# marks=int(input("Enter your Marks: "))
+
+# if marks>=90:
+#     print("grade A")
+
+# elif marks>=75 :
+#     print("grade B")
+
+# elif marks>=50:
+#     print("grade C")
+
+# else:
+#     print("fail")
+
+
+
+from dev.config import Password
+# print("welcome to the magic show ")
+
+# guess=int(input("guess a number between 1 to 10:"))
+
+# if guess == secret_number:
+#     print("you guessed it right")
+# else:
+#     print("you guessed it wrong")
+
+password=int(input("enter your password"))
+if password ==Password:
+    print("access granted")
+else:
+    print("access denied")
---
+++ Introduction of python/Day1/day5/dev/config.py
@@ -0,0 +1,2 @@
+secret_number = 7
+Password=123123
