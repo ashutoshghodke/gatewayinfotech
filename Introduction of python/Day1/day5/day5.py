d30f9ca7633a75ec224e593aacf0c76484cd7c59
ashutoshghodke <ashutoshghodke5600@gmail.com>
2025-07-23 15:27:10 +0530

    1000 problems

--- Introduction of python/Day1/day5/day5.py
+++ Introduction of python/Day1/day5/day5.py
@@ -30,7 +30,7 @@
 # else:
 #     print("fail")
 
-# from dev.config import Password,USERNAME,secret_number
+ from dev.config import Password,USERNAME,secret_number
 
 # print("welcome to the magic show ")
 
@@ -48,37 +48,4 @@
 # else:
 #     print("access denied")
 """
-import random
 
-secret_number = random.randint(1, 50)
-print("Welcome to the Number Guessing Game!")
-print("Guess the number between 1 and 50.")
-print("You have 5 attempts. Enter 0 to quit anytime.")
-
-for attempt in range(1, 6):  # 5 chances
-    guess = int(input(f"Attempt {attempt}: Enter your guess: "))
-
-    if guess == 0:
-        print("Game stopped by player.")
-        break  # Exit the game immediately
-    
-    if guess < 0:
-        print("Negative numbers are not allowed! Attempt skipped.")
-        continue  # Skip this attempt
-
-    if guess == secret_number:
-        print(f"Congratulations! You guessed the number in {attempt}th attempts.")
-        break  # Player wins, exit loop
-    
-    elif guess > secret_number:
-        print("Too high! Try again.")
-    
-    else:
-        print("Too low! Try again.")
-
-    if attempt == 3:
-        pass  # Placeholder: Add a hint feature here later
-
-else:
-    # This runs only if the for loop completes without a break
-    print(f"Sorry, you're out of attempts. The number was {secret_number}.")
