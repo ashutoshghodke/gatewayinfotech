db50447ce312459226b6dca468a623a2b3ed8025
ashutoshghodke <ashutoshghodke5600@gmail.com>
2025-08-15 14:30:02 +0530

    class day 8

---
+++ Introduction of python/Day1/day8/day8.py
@@ -0,0 +1,85 @@
+
+# empty_tuple = ()
+# print(empty_tuple)
+# empty_tuple2 = tuple()
+# print(empty_tuple2)
+# print(type(empty_tuple2))
+
+
+# lst=[1,2,3,4,5]
+# tup=tuple[1,2,3,4,5]
+# print(type(tup))
+# print(type(lst))
+
+# tup=(1,2,3,4)
+# l1=list(tup)
+# print(type(l1))
+
+
+
+# fruits = ("apple", "banana", "cherry")
+
+# print(fruits[-1])  # Output: apple
+
+# print(fruits[2])  # Output: cherry
+
+# print(fruits[-1]) # Output: cherry (counting from the end)
+
+
+
+# votes = ("yes", "no", "yes", "yes")
+
+# yes_votes = votes.count("yes")
+# print(yes_votes)
+# no_votes = votes.count("no")
+# print(no_votes)
+
+# pets = ["dog", "cat", "fish", "cat",10,22,"fish"]
+# cat_index = pets.index("fish")
+# print(cat_index)
+
+# colors = ("red", "green", "blue", "green")
+# green_index = colors.index("green")
+# print(green_index) # Output: 1
+
+
+# coordinates = (10, 20)
+
+# x, y = coordinates
+
+# print(x) # Output: 10
+
+# print(y) # Output: 20
+
+# clas= ("sanket","pradeep","rohini")
+# a,b,c=clas
+# print(a)
+# print(b)
+# print(c)
+
+
+# personal_info = ("John", 30)
+
+# location = ("New York", "USA")
+
+# full_profile = personal_info + location
+# print(full_profile)
+
+
+# short_tuple = (12)
+# short_tuple = [12]
+# long_tuple = short_tuple * 3
+# print(long_tuple)
+
+
+# weekdays = ["Monday", "Tuesday", "Wednesday"]
+# weekdays = ("Monday", "Tuesday", "Wednesday")
+# print("Monday" in weekdays) # Output: True
+
+# print("Sunday" in weekdays) # Output: False
+
+
+
+# # Nested Tuples:
+# nested = ((1, 2), (3, 4))
+# print(nested[1][0])  # 2
