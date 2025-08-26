7fff5d9b7e8868d45b2b683e366959f1abf30136
ashutoshghodke <ashutoshghodke5600@gmail.com>
2025-08-26 21:38:13 +0530

    classs12

---
+++ Introduction of python/Days/12day12/day12.py
@@ -0,0 +1,43 @@
+# def welcome():
+#     print("Welcome to Python class!")
+
+# # Calling the function 3 times
+# welcome()
+# welcome()
+# welcome()
+# welcome()
+# welcome()
+# welcome()
+# # welcome()
+
+# def greet():
+#     print("Hello, How are you?")
+
+# greet()   # calling the function
+# greet() 
+# greet() 
+
+# def std():
+#     print("hithere!")
+
+# std()
+
+
+# def greet(name):
+#     print("Hello", name, "How are you?")
+
+# greet("Amit")
+# greet("Sita")
+# greet("sanket")
+
+# def add(a, b):
+#     return a + b
+
+# result = add(54,33)
+# print("Sum is:", result)
+
+def order_pizza(flavour="Margherita"):
+    print("You ordered", flavour, "pizza!")
+
+order_pizza(input("enter ur flavour"))
+order_pizza()   # default will be Margherita
